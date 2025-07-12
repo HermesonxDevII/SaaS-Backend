dba259f2ece4641de10909069749ed392d112cd5
HermesonxDev <hermesoninfo@gmail.com>
2025-07-12 16:10:27 -0300

    fix: tests error correction

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,13 +1,19 @@
 <?php
 
+use App\Models\User;
+
 test('registration screen can be rendered', function () {
-    $response = $this->get('/register');
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->get('/register');
 
     $response->assertStatus(200);
 });
 
 test('new users can register', function () {
-    $response = $this->post('/register', [
+    $user = User::factory()->create();
+    
+    $response = $this->actingAs($user)->post('/register', [
         'name' => 'Test User',
         'email' => 'test@example.com',
         'password' => 'password',
